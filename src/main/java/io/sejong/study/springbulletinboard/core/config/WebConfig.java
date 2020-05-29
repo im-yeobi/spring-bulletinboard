f5acadf62ec809a6293516834aedfadc6f52f9bf
im-yeobi <dev.jongyeobKim@gmail.com>
2020-05-29 13:55:37 +0900

    Change config package structure

--- src/main/java/io/sejong/study/springbulletinboard/core/config/WebConfig.java
+++ src/main/java/io/sejong/study/springbulletinboard/core/config/WebConfig.java
@@ -1,4 +1,4 @@
-package io.sejong.study.springbulletinboard.common.config;
+package io.sejong.study.springbulletinboard.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
